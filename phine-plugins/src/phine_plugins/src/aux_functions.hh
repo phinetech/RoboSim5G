9d47f34aad8dc9c2a92614a1c6bb1e3ccd17db33
phine-richiB <riccardo.belletti@phine.tech>
2025-07-09 15:23:36 +0200

    fix: correct licence label

--- phine-plugins/src/phine_plugins/src/aux_functions.hh
+++ phine-plugins/src/phine_plugins/src/aux_functions.hh
@@ -1,4 +1,4 @@
-/* Copyright 2025 RoboSim5G
+/* Copyright 2025 phine.tech GmbH
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.

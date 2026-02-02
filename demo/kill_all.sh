bf6f6851ec464073e15181a143119577c2a41526
Riccardo <riccardo.belletti@phine.tech>
2026-02-02 16:21:55 +0100

    fix: changing IGN_TRANSPORT_INTERFACE with IGN_IP, adding call to ros_gz_bridge in launch_robot.sh

--- demo/kill_all.sh
+++ demo/kill_all.sh
@@ -1,8 +1,8 @@
 pkill -f ign\ gazebo
-
 cd oai_setup
 docker compose -f docker-compose-ue.yml down
 docker ps -q --filter ancestor=oaisoftwarealliance/oai-gnb:2024.w44 | xargs -I {} docker stop {} && docker ps -a -q --filter ancestor=oaisoftwarealliance/oai-gnb:2024.w44 | xargs -I {} docker rm {}
 docker compose down
+docker network rm ros_gz_net
 xhost -local:docker
 cd ..
